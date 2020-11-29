acd06ed183d592d177f4377b666ba62189dfb0b9
Michael Thompson <michael.220488@gmail.com>
2020-11-28 16:53:30 -0800

    created sample list

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,7 +3,34 @@ import {
   List
 } from 'react-virtualized'
 import axios from 'axios'
+import faker from 'faker'
 
-export default App = () => {
-  return <div> Hello, world </div>;
+const App = () => {
+  const [cards, setCards] = React.useState([]);
+  React.useEffect(()=>{
+    setCards(
+      [...Array(100).keys()].map((key) =>{
+        return {
+          id: key,
+          name: `${faker.name.firstName()}`,
+          bio: faker.lorem.lines(Math.random()*100)
+        }
+      })
+    )
+  }, []);
+  return (
+  <div> 
+    <ul>
+      {
+        cards.map((card) => (
+          <li key= {card.id}>
+            <h2>{card.name}</h2>
+          </li>
+          )
+        )
+      }
+    </ul>
+  </div>
+  );
 }
+export default App
